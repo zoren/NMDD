cd71bf2bd3cd513f0b74b902f2961bcc5863c866
Søren Sjørup <zorren@gmail.com>
2017-05-16 23:34:25 +0200

    renamed spec

--- spec/single_var_bdd_spec.ts
+++ spec/single_var_bdd_spec.ts
@@ -29,11 +29,18 @@ describe("single variable BDD", () => {
         expect(b.Make(0, [1, 1])).toBe(1);
     });
 
-    it("should", () => {
+    it("should return static conjunction", () => {
         let n1 = b.Make(0, [0, 1]);
         let n2 = b.Make(0, [1, 0]);
-        let r = b.ApplyN((ns) => ns[0] === 1 && ns[1] === 1 ? 1 : 0, [n1, n2]);
+        let r = b.ApplyN(ns => ns[0] === 1 && ns[1] === 1 ? 1 : 0, [n1, n2]);
         expect(r).toBe(0);
+    });
+
+    it("should return static disjunction", () => {
+        let n1 = b.Make(0, [0, 1]);
+        let n2 = b.Make(0, [1, 0]);
+        let r = b.ApplyN(ns => ns[0] === 1 || ns[1] === 1 ? 1 : 0, [n1, n2]);
+        expect(r).toBe(1);
     });
 
 });
